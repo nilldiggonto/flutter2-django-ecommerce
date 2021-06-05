5d9bb14aca955c4e341e5f9cd77ef0d480b7617f
nilldiggonto <nilldiggonto@gmail.com>
2021-06-05 23:56:50 +0600

    Basic Auth done

--- flower/serializers.py
+++ flower/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
+
 class ProductSerializers(serializers.ModelSerializer):
     class Meta:
         model = Product
--- flower/views.py
+++ flower/views.py
@@ -3,8 +3,12 @@ from .serializers import *
 from .models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 # Create your views here.
 class ProductView(APIView):
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [TokenAuthentication,]
     def get(self,request):
         queryset = Product.objects.all()
         serializer = ProductSerializers(queryset,many=True)
